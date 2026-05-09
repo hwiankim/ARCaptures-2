2c4f88f119b32d15a630c2959b41171859c0a545
김휘안 <gnlxo55@gmail.com>
2026-05-09 22:42:20 +0900

    Initial Commit

---
+++ ARFoodCapture2/ARFoodCapture2App.swift
@@ -0,0 +1,17 @@
+//
+//  ARFoodCapture2App.swift
+//  ARFoodCapture2
+//
+//  Created by 김휘안 on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ARFoodCapture2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
